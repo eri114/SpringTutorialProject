804498711ff1692827b4ef1047be91079ac6399b
eri114 <erimemo0114@gmail.com>
2020-03-14 22:33:46 +0900

    悲観的ロック処理追加

--- src/main/java/project/tutorial/spring/domain/repository/room/ReservableRoomRepository.java
+++ src/main/java/project/tutorial/spring/domain/repository/room/ReservableRoomRepository.java
@@ -3,12 +3,19 @@ package project.tutorial.spring.domain.repository.room;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import project.tutorial.spring.domain.model.ReservableRoom;
 import project.tutorial.spring.domain.model.ReservableRoomId;
 
 public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, ReservableRoomId> {
+	
+	// 予約する会議室を悲観的ロック
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	ReservableRoom findOneForUpdateByReservableRoomId(ReservableRoomId reservableRoomId);
 	
 	// 指定日に予約可能な会議室の一覧を取得するメソッド
 	List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(
--- src/main/java/project/tutorial/spring/domain/service/reservation/ReservationService.java
+++ src/main/java/project/tutorial/spring/domain/service/reservation/ReservationService.java
@@ -30,7 +30,7 @@ public class ReservationService {
 		ReservableRoomId reservableRoomId = reservation.getReservableRoom().getReservableRoomId();
 		
 		// 対象の部屋が予約可能かをチェック
-		ReservableRoom reservable = reservableRoomRepository.findById(reservableRoomId).get();
+		ReservableRoom reservable = reservableRoomRepository.findOneForUpdateByReservableRoomId(reservableRoomId);
 		
 		if(reservable == null) {
 			throw new UnavailableReservationException("入力の日付・部屋の組み合わせは予約できません。");
